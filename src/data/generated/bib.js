5c532cf2e403a9ce8095d63920e19d45553c390d
tanzzy7724 <tanzy3850@163.com>
2025-05-14 17:54:27 +0800

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿const generatedBibEntries = {
+    "Cai2020PSO": {
+        "author": "L. Cai and Y. Hou and Y. Zhao and J. Wang",
+        "booktitle": "2020 IEEE International Conference on Power, Intelligent Computing and Systems (ICPICS)",
+        "doi": "10.1109/ICPICS50287.2020.9202023",
+        "keywords": "type:strategy,particle swarm optimization algorithm,Application research,improvement",
+        "pages": "238--241",
+        "series": "ICPICS",
+        "title": "Application research and improvement of particle swarm optimization algorithm",
+        "type": "inproceedings strategy",
+        "year": "2020"
+    },
+    "He2023PSO": {
+        "author": "Y. He and M. Xin and Y. Wang and C. Xu",
+        "booktitle": "2023 3rd International Conference on New Energy and Power Engineering (ICNEPE)",
+        "doi": "10.1109/ICNEPE60694.2023.10429415",
+        "keywords": "type:architecture,Improve particle swarm optimization,Integrated Circuit,global cabling,fitness function",
+        "pages": "737--741",
+        "series": "ICNEPE",
+        "title": "A Global Routing Optimization Method for Integrated Circuit Clock Network Based on Improved Particle Swarm Optimization",
+        "type": "inproceedings architecture",
+        "year": "2023"
+    },
+    "Hu2018PSO": {
+        "author": "K. Hu and G.-L. Zhang and B. Xiong",
+        "booktitle": "2018 International Conference on Machine Learning and Cybernetics (ICMLC)",
+        "doi": "10.1109/ICMLC.2018.8526956",
+        "keywords": "type:strategy,Particle swarm algorithm, constraint optimization problem,violation of constraints",
+        "pages": "393--398",
+        "series": "ICMLC",
+        "title": "An Improved Particle Swarm Algorithm for Constrained Optimization Problem",
+        "type": "inproceedings strategy",
+        "year": "2018"
+    },
+    "Na2024PSO": {
+        "author": "S. Na and M. Xuan",
+        "booktitle": "2024 20th International Conference on Natural Computation, Fuzzy Systems and Knowledge Discovery (ICNC-FSKD)",
+        "doi": "10.1109/ICNC-FSKD64080.2024.10702232",
+        "keywords": "type:architecture,particle swarm optimization, compute Unified Device Architecture,Symbolic Regression",
+        "pages": "1--6",
+        "series": "ICNC-FSKD",
+        "title": "An Improved Particle Swarm Optimization Algorithm Based Computer Unified Device Architecture to Symbolic Regression",
+        "type": "inproceedings architecture",
+        "year": "2024"
+    },
+    "Qingling2023pso": {
+        "author": "W. Qingling and J. Yubo",
+        "booktitle": "2023 International Conference on Computers, Information Processing and Advanced Education (CIPAE)",
+        "doi": "10.1109/CIPAE60493.2023.00075",
+        "keywords": "type:hybrid,Improved particle swarm optimization, Annealing algorithm, parameter optimization",
+        "pages": "360--362",
+        "series": "CIPAE",
+        "title": "Research on Improved Particle Swarm Optimization Algorithm Based on Simulated Annealing Algorithm",
+        "type": "inproceedings hybrid",
+        "year": "2023"
+    },
+    "Shami2022survey": {
+        "author": "T. M. Shami and A. A. El-Saleh and M. Alswaitti and Q. Al-Tashi and M. A. Summaikeh and S. Mirjalili",
+        "booktitle": "IEEE Access",
+        "doi": "10.1109/ACCESS.2022.3142859",
+        "keywords": "type:survey,Applications of PSO, binary PSO, evolutionary Computation,feature selection, hybrid algorithms, meta-heuristic algorithm, particle swarm optimization,PSO variants",
+        "pages": "10031--10061",
+        "series": "IEEE Access",
+        "title": "Particle Swarm Optimization: A Comprehensive Survey",
+        "type": "inproceedings survey",
+        "year": "2022"
+    },
+    "Shilpa2016PSO": {
+        "author": "K. C. Shilpa and C. Lakshminarayana and M. K. Singh",
+        "booktitle": "2016 International Conference on Communication and Electronics Systems (ICCES)",
+        "doi": "10.1109/CESYS.2016.7889884",
+        "keywords": "type:parameter_tuning,Adaptive particle swarm optimization, algorithmic level synthesis,evolution computation,optimal schedule, particle swarm optimization, second order differential equation solver",
+        "pages": "1--5",
+        "series": "ICCES",
+        "title": "Adaptive particle swarm optimization for best schedule in algorithmic-level synthesis",
+        "type": "inproceedings parameter_tuning",
+        "year": "2016"
+    },
+    "Wu2015iqPSO": {
+        "author": "J.-Y. Wu",
+        "booktitle": "2015 15th International Symposium on Communications and Information Technologies (ISCIT)",
+        "doi": "10.1109/ISCIT.2015.7458331",
+        "keywords": "type:parameter_tuning,particle swarm optimization, constrained global optimization, Quantum Computing",
+        "pages": "157--160",
+        "series": "ISCIT",
+        "title": "An improved quantum-behaved particle swarm optimization method for solving constrained global optimization problems",
+        "type": "inproceedings parameter_tuning",
+        "year": "2015"
+    },
+    "Yuan2021PSO": {
+        "author": "Z. Yuan and J. Li and H. Yang and B. Zhang",
+        "booktitle": "2021 IEEE International Conference on Progress in Informatics and Computing (PIC)",
+        "doi": "10.1109/PIC53636.2021.9687017",
+        "keywords": "type:hybrid,Whale optimization algorithm, meeta-heuristic algorithm,particle swarm optimization algorithm,explpration and exploitation",
+        "pages": "260--264",
+        "series": "PIC",
+        "title": "A Hybrid Whale Optimization and Particle Swarm Optimization Algorithm",
+        "type": "inproceedings hybrid",
+        "year": "2021"
+    },
+    "kennedy1995PSO": {
+        "author": "J. Kennedy and R. Eberhart",
+        "booktitle": "Proceedings of ICNN'95 - International Conference on Neural Networks",
+        "doi": "10.1109/ICNN.1995.488968",
+        "keywords": "type:original,swarm_intelligence",
+        "pages": "1942--1948",
+        "series": "ICNN",
+        "title": "Particle swarm optimization",
+        "type": "inproceedings original",
+        "year": "1995"
+    }
+};
